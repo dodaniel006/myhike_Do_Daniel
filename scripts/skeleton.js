aee12a50396177c9726facc302a25700cd033079
Daniel Do <dodaniel006@gmail.com>
2025-04-01 09:25:01 -0700

    Fix navbar text

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -9,10 +9,12 @@ function loadSkeleton() {
 		        // If the "user" variable is not null, then someone is logged in
             // User is signed in.
             // Do something for the user here.
+            console.log("logged in");
             console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer.html'));
         } else {
             // No user is signed in.
+            console.log("not logged in");
             console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer.html'));
         }
